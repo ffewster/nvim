edf1b6b1584b1c89b9f11f8b155c715f248f0d79
Fraser Fewster <ffewster@twigeducation.com>
2024-05-02 12:25:00 +0100

    Enable copilot by default

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,21 +45,7 @@ return {
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
-        build = ":Copilot auth",
-        opts = {
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-            },
-            panel = {
-                enabled = true,
-                auto_refresh = true,
-            },
-            filetypes = {
-                ["*"] = true,
-                typescript = true
-            }
-        },
+        event = "InsertEnter",
         config = function()
             require("copilot").setup({
                 suggestion = {
