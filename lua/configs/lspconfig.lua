653719b8822f53d5e91078f694461115861cb296
Fraser Fewster <ffewster@twigeducation.com>
2024-05-23 15:42:29 +0100

    Add better hover info for lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -21,3 +21,13 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 500
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
