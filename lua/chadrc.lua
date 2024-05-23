33a654d66b0f5db362377995a85a634073fee70c
Fraser Fewster <ffewster@twigeducation.com>
2024-05-23 11:00:58 +0100

    Add devcontainer plugin

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,12 @@
 local M = {}
 
 M.ui = {
-	theme = "gruvchad",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	theme = "gruvbox",
+    transparency = true,
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -80,6 +80,14 @@ return {
         },
         -- See Commands section for default commands if you want to lazy load on them
     },
-
-
+    -- {
+    --     "jamestthompson3/nvim-remote-containers",
+    --     config = function()
+    --         require("nvim-remote-containers").setup()
+    --     end
+    -- }
+    {
+        'https://codeberg.org/esensar/nvim-dev-container',
+        dependencies = 'nvim-treesitter/nvim-treesitter'
+    }
 }
