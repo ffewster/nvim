998fc90422da3aef4753f66f3ef2524bad3cff11
Fraser Fewster <ffewster@twigeducation.com>
2024-05-29 10:50:34 +0100

    Add useful comment shortcutting

--- init.lua
+++ init.lua
@@ -320,6 +320,24 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "numToStr/Comment.nvim",
+    config = function(_, opts)
+      require("Comment").setup(opts)
+
+      vim.keymap.set("n", "<leader>/", function()
+        require("Comment.api").toggle.linewise.current()
+      end, { desc = "comment toggle" })
+
+      vim.keymap.set(
+        "v",
+        "<leader>/",
+        "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+        { desc = "comment toggle" }
+      )
+    end,
+  },
+
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
