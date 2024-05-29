20ec033a01492dabd1b701c6d5c116c0e9c09fd4
Fraser Fewster <ffewster@twigeducation.com>
2024-05-29 17:27:38 +0100

    Add copilot

--- init.lua
+++ init.lua
@@ -346,6 +346,31 @@ require("lazy").setup({
     -- this is equalent to setup({}) function
   },
 
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<M-l>",
+          },
+        },
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+        },
+        filetypes = {
+          ["*"] = true,
+          typescript = true,
+        },
+      }
+    end,
+  },
+
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
