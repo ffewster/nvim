3826bb806b8840b83d47739cf0bb4b060d5ef0dd
Fraser Fewster <fraser.fewster@imaginelearning.com>
2025-04-07 13:31:05 +0100

    Add close tab motion

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -178,9 +178,12 @@ return {
       "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
     },
     init = function()
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
       vim.g.barbar_auto_setup = false
-      vim.api.nvim_set_keymap("n", "<Tab>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
+      map("n", "<Tab>", "<Cmd>BufferNext<CR>", opts)
+      map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", opts)
+      map("n", "<leader>x", "<Cmd>BufferClose<CR>", opts)
     end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
